fd3d583867f12da49edc846b0f3b5b8087de90ef
dabin <duobin0626@gmail.com>
2023-07-07 12:58:55 +0900

    DB연결완료
    DB연결완료

--- clone/carrot/main.py
+++ clone/carrot/main.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI,UploadFile,Form
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
+import sqlite3
+
+con = sqlite3.connect('carrot.db',check_same_thread=False)
+cur = con.cursor()
 
 app = FastAPI()
 
 #프론트엔드에서 받을 값 , 각 각 타입 지정필요.
 @app.post('/items')
-def create_item(image:UploadFile, 
+async def create_item(image:UploadFile, 
                 title:Annotated[str,Form()], 
                 price:Annotated[int,Form()], 
                 description:Annotated[str,Form()], 
                 place:Annotated[str,Form()]):
-    print(image, title, price, description, place)
+    
+    # image_bytes : 이미지의 크기가 크기때문에 읽기위한 시간이 필요하여 처리 (async await)
+    image_bytes = await image.read()
+    cur.execute(f"""
+                INSERT INTO items(title,image, price, description, place)
+                VALUES('{title}','{image_bytes.hex()}',{price},'{description}','{place}')
+                """)    
+    print(image, title, price, description, place) # 데이터값 확인용도
+    con.commit() # 데이터가 들어감.
     return'200' # 200 = ok
 
 
